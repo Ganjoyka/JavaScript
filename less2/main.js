5ca0fff8c29cf37415dc0c3fd057e6f809228a68
Ganjoyka <vladmudrenko7@gmail.com>
2022-11-08 22:32:26 +0200

    test

---
+++ less2/main.js
@@ -0,0 +1,106 @@
+// Zavd 1
+let arr = [1, 2, `255`, true, -25, `vi`, 12, 1525, 0, 12222]
+console.log(arr);
+
+// Zavd 2
+let book1 = {
+    title: `Bolt`,
+    pageCount: 10,
+    genre: `oli`
+}
+console.log(book1);
+let book2 = {
+    title: `Gala`,
+    pageCount: 100,
+    genre: `ila`
+}
+console.log(book2)
+let book3 = {
+    title: `Gem`,
+    pageCount: 50,
+    genre: `Gio`
+}
+console.log(book3)
+
+// Zavd 3
+let book = {
+    title: `All`,
+    pageCount: 10,
+    genre: `Yms`,
+    authors: [
+        {
+            name: `Jack`,
+            age: 15,
+        },
+        {
+            name: `Marina`,
+            age: 25,
+        },
+        {
+            name: `Anna`,
+            age: 52,
+        }
+    ]
+}
+console.log(book)
+let books = {
+    title: `Halli`,
+    pageCount: 47,
+    genre: `Hage`,
+    authors: [
+        {
+            name: `Roki`,
+            age: 54
+        },
+        {
+            name: `Arina`,
+            age: 88,
+        },
+        {
+            name: `Hal`,
+            age: 45,
+        }
+    ]
+}
+console.log(books)
+let book0 = {
+    title: `Galli`,
+    pageCount: 123,
+    genre: `Mon`,
+    authors: [
+        {
+            name: `Lili`,
+            age: 72,
+        },
+        {
+            name: `Luna`,
+            age: 35,
+        },
+        {
+            name: `Dany`,
+            age: 19,
+        }
+    ]
+}
+console.log(book0)
+
+// Zavd 4
+let arr1 = [
+    {
+        name: "Manya",
+        username: "mai1",
+        password: 1234
+    },  {
+        name: "Dara",
+        username: "KIKI11",
+        password: 89456
+    },  {
+        name: "Nasi",
+        username: "Pals777",
+        password: 4545
+    },
+]
+console.log(arr1[0].password)
+console.log(arr1[1].password)
+console.log(arr1[2].password)
+console.log(arr1[3].password)
