96b011bf886092970e76875c281b9b2ee0a3427f
Ganjoyka <vladmudrenko7@gmail.com>
2022-11-08 22:43:26 +0200

    less3

---
+++ less3/main.js
@@ -0,0 +1,39 @@
+// Zavdannya 1
+let a = `hello`
+let s = `own`
+let d = `com`
+let g = `ua`
+let h = `1`
+let j = `10`
+let k = `-999`
+let l = `123`
+let z = `3.14`
+let x = `2.7`
+let c = `16`
+let t = `true`
+let f = `false`
+console.log(a, s, d, f, g, t, h, j, k, l, z, x, c)
+
+// Zavdannya 2
+let firstName = `Mudrenko`
+let middleName = `Vladyslav`
+let lastName = `Borysovych`
+
+let person = firstName + ` ` + middleName + ` ` + lastName
+console.log(person)
+
+// Zavdannya 3
+let aa = 100
+console.log(typeof aa)
+let bb = `100`
+console.log(typeof bb)
+let cc = true
+console.log(typeof cc)
+
+// Zavdannya 4
+let firstName1 = prompt(`firstName1`);
+let lastName1 = prompt(`lastName1`);
+let age = prompt(`age`);
+console.log(firstName1)
+console.log(lastName1)
+console.log(age)
